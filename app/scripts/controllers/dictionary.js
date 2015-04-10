3eccad732be00b1fb596386f5490d59966a28692
Andrey Vasilenko <0933322888@ukr.net>
2015-04-11 01:59:29 +0300

    dictionaries are not displayed on the main view

--- app/scripts/controllers/dictionary.js
+++ app/scripts/controllers/dictionary.js
@@ -5,58 +5,49 @@ angular.module('admissionSystemApp')
   .controller('dictionaryCtrl', ['$scope', 'ngTableParams', 'SpecofferDictionaryService',
     function ($scope, NgTableParams, SpecofferDictionaryService) {
 
-      $scope.dataNew=[];
+      $scope.newData=[];
 
       $scope.dictionaries = [
-        {name: 'num', display: 'Номер', headers: {}, id:0},
-        {name: 'num', display: 'Номер', headers: {}, id:0},
-        {name: 'num', display: 'Номер', headers: {}, id:0},
-        {name: 'num', display: 'Номер', headers: {}, id:0},
-        {name: 'num', display: 'Номер', headers: {}, id:0},
-        {name: 'num', display: 'Номер', headers: {}, id:0},
-        {name: 'num', display: 'Номер', headers: {}, id:0, dict},
-        {name: 'num', display: 'Номер', headers: {}, id:0},
-        {name: 'num', display: 'Номер', headers: {}, id:0},
-        {name: 'num', display: 'Номер', headers: {}, id:0},
-        {name: 'num', display: 'Номер', headers: {}, id:0},
-        {name: 'num', display: 'Номер', headers: {}, id:0},
-        {name: 'num', display: 'Номер', headers: {}, id:0},
-        {name: 'num', display: 'Номер', headers: {}, id:0},
-        {name: 'num', display: 'Номер', headers: {}, id:0},
-        {name: 'num', display: 'Номер', headers: {}, id:0},
-        {name: 'num', display: 'Номер', headers: {}, id:0},
-        {name: 'num', display: 'Номер', headers: {}, id:0},
-        {name: 'num', display: 'Номер', headers: {}, id:0},
-        {name: 'num', display: 'Номер', headers: {}, id:0},
-        {name: 'num', display: 'Номер', headers: {}, id:0},
-        {name: 'num', display: 'Номер', headers: {}, id:0},
-        {name: 'num', display: 'Номер', headers: {}, id:0},
-        {name: 'num', display: 'Номер', headers: {}, id:0},
-        {name: 'num', display: 'Номер', headers: {}, id:0}
+        {name: 'Довідник типів часових періодів', headers: {}, id:1},
+        {name: 'Довідник типів спеціальностей', headers: {}, id:2},
+        {name: 'Напрями та спеціальності', headers: {}, id:3},
+        {name: 'Інформація про підрозділи', headers: {}, id:4},
+        {name: 'Тип пропозиції', headers: {}, id:5},
+        {name: 'Довідник форм навчання', headers: {}, id:6},
+        {name: 'Довідник типів пільг', headers: {}, id:7},
+        {name: 'Дані про пільги', headers: {}, id:8},
+        {name: 'Довідник типу персон', headers: {}, id: 9},
+        {name: 'Довідник статі персон', headers: {
+          abbrName: 'Скорочена назва',
+          name: 'Hазва'
+          }, id: 10, dict: function() {SpecofferDictionaryService.getGenderTypes().then(function (data){$scope.newData = data; console.log('data:',data); console.log('scope.dics:',$scope.dictionaries[$scope.dictionary-1].headers)})}},
+        {name: 'Довідник сімейного стану', headers: {}, id:11},
+        {name: 'Довідник типів адмістративно-територіальних одиниць', headers: {}, id:12},
+        {name: 'Адміністративно-територіальні одиниці', headers: {}, id:13},
+        {name: 'Довідник типу адрес', headers: {}, id:14},
+        {name: 'Довідник типів контактів', headers: {}, id:15},
+        {name: 'Довідник типу публічних активностей', headers: {}, id:16},
+        {name: 'Нагороди за публічні заходи', headers: {}, id:17},
+        {name: 'Публічні активності', headers: {}, id:18},
+        {name: 'Довідник придметів ЗНО', headers: {}, id:19},
+        {name: 'Довідник типів відзнак', headers: {}, id:20},
+        {name: 'Довідник типів використання документів', headers: {}, id:21},
+        {name: 'Довідник типів документів', headers: {}, id:22},
+        {name: 'Довідник типів поступлень', headers: {}, id:23},
+        {name: 'Довідник типів статусу заявки', headers: {}, id:24},
+        {name: 'Часові періоди', headers: {}, id:25}
       ];
+      $scope.dictionary = $scope.dictionaries[0];
 
 
+      $scope.pickDictionary = function () {
+        $scope.dictionaries[($scope.dictionary)-1].dict()
+        };
 
-
-
-
-
-
-      SpecofferDictionaryService.getTimeperiods({timePeriodTypeId: 1}).then(function (timeperiods) {
-        $scope.timeperiods = timeperiods;
-      });
-      $scope.dataNew = [];
-      $scope.pickTimePeriod = function () {
-        valueSendingService.timeperiod = $scope.timeperiod.timePeriodId;
-        SpecofferDictionaryService.clearStorageByRoute('specoffers');
-        ListProposalGettingService.allProposalsDecoded($scope.timeperiod).then(function (data) {
-          $scope.dataNew = data;
-        });
-      };
       var getData = function () {
-        return $scope.dataNew;
+        return $scope.newData;
       };
-      $scope.$watch('dataNew', function () {
+      $scope.$watch('newData', function () {
         $scope.tableParams.reload();
       });
       $scope.tableParams = new NgTableParams({
@@ -68,9 +59,9 @@ angular.module('admissionSystemApp')
         }, // length of data
         getData: function ($defer, params) {
           var moreData = getData();
-          moreData.forEach(function (el, index) {
-            el.num = index + 1;
-          });
+
+
+
           params.total(moreData.length);
           $defer.resolve(moreData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
         }
