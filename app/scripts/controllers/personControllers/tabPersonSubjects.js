b324ffa9880c52159c140b074e9a48a18272d0d1
Andrey <0933322888@ukr.net>
2015-04-22 21:02:09 +0300

    tabs Subjects workflow2

--- app/scripts/controllers/personControllers/tabPersonSubjects.js
+++ app/scripts/controllers/personControllers/tabPersonSubjects.js
@@ -22,26 +22,31 @@ angular.module('admissionSystemApp')
 
 
       $scope.decodedSubjects = [];
-      //$scope.decodedObj = {};
 
     // create dictionary with subjects
     $scope.enrolSubjIds = SpecofferDictionaryService.getEnrolmentsSubjects().then(function(subjs) {
       $scope.subjs = subjs;
-      $scope.arr = [];
+      $scope.dictionaryArr = [];
       angular.forEach(subjs, function(subjs){
-        $scope.arr[subjs.id] = subjs.name;
+        $scope.dictionaryArr[subjs.id] = subjs.name;
       });
-      console.log('array of subject names:',$scope.arr);
+      $scope.viewDictionary = angular.copy($scope.subjs);
+      console.log('viewDictionary:',$scope.dictionaryArr);
 
       if ($scope.entirePerson.enrolmentsubjects) {
         angular.forEach($scope.entirePerson.enrolmentsubjects, function (decodedObj) {
           var temp = {};
           temp.id = decodedObj.id;
           temp.personPaperId = decodedObj.personPaperId;
-          temp.enrolmentSubjectId = $scope.arr[decodedObj.enrolmentSubjectId];
+          temp.enrolmentSubjectId = $scope.dictionaryArr[decodedObj.enrolmentSubjectId];
           temp.mark = decodedObj.mark;
           $scope.decodedSubjects.push(temp);
+
+          $scope.viewDictionary.splice($scope.dictionaryArr.indexOf(temp.enrolmentSubjectId)-1, 1);
+          //console.log('subj:', $scope.dictionaryArr.indexOf(temp.enrolmentSubjectId));
+          console.log('viewDict', $scope.viewDictionary);
         });
+
       }
     });
 
@@ -62,15 +67,20 @@ angular.module('admissionSystemApp')
       newSubj.enrolmentSubjectId = $scope.enrolmentsubject.id;
       newSubj.mark = $scope.subjectMark;
       $scope.entirePerson.enrolmentsubjects.push(angular.copy(newSubj));
-      console.log('subject before decoded', $scope.enrolmentsubject.id);
+      console.log('subject before decoded', $scope.enrolmentsubject.dict);
 
-
-
-      newSubj.enrolmentSubjectId = $scope.enrolmentsubject.name;
+      newSubj.enrolmentSubjectId = $scope.dictionaryArr[$scope.enrolmentsubject.id];
       $scope.decodedSubjects.push(newSubj);
 
-      console.log('subject after decoded', $scope.enrolmentsubject.name);
-      console.log('subject in decoded', $scope.decodedSubjects);
+      // problem is here
+      $scope.viewDictionary.splice($scope.dictionaryArr.indexOf($scope.enrolmentsubject.name), 1);
+
+          console.log('AAAAAAAA---', $scope.dictionaryArr.indexOf($scope.enrolmentsubject.name));
+          console.log('viewDict', $scope.viewDictionary);
+
+
+      //console.log('subject after decoded', $scope.enrolmentsubject.name);
+      //console.log('subject in decoded', $scope.decodedSubjects);
       console.log('subject in entirePerson', $scope.entirePerson.enrolmentsubjects);
     };
 
@@ -93,14 +103,15 @@ angular.module('admissionSystemApp')
         scope: $scope,
         controller: function ($rootScope, $scope, $modalInstance, id) {
           $scope.id = id;
+          $scope.modalMark = $scope.decodedSubjects[id-1].mark;
 
           $scope.ok = function (id) {
             $scope.entirePerson.enrolmentsubjects[id-1].enrolmentSubjectId = $scope.modalEnrolmentSubject.id;
             $scope.entirePerson.enrolmentsubjects[id-1].mark = $scope.modalMark;
             $scope.decodedSubjects[id-1].enrolmentSubjectId = $scope.modalEnrolmentSubject.name;
             $scope.decodedSubjects[id-1].mark = $scope.modalMark;
-            console.log('entirePerson', $scope.entirePerson.enrolmentsubjects);
-            console.log('decoded', $scope.decodedSubjects);
+            //console.log('entirePerson', $scope.entirePerson.enrolmentsubjects);
+            //console.log('decoded', $scope.decodedSubjects);
 
             $modalInstance.close();
           };
