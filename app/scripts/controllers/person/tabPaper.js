bb6516ca2deacd4bf87994aea93b6f595a4bbc33
dmitry <alice_ld@ukr.net>
2015-05-14 20:20:31 +0300

    tabPapers oneWay

--- app/scripts/controllers/person/tabPaper.js
+++ app/scripts/controllers/person/tabPaper.js
@@ -10,7 +10,13 @@ angular.module('admissionSystemApp')
         isCreating: true,
         isAddToTable: true,
         publicActiveTable: false,
-        publicActivSelect: false
+        publicActiveSelect: false,
+        studyTable: false,
+        studySelect: false,
+        docPinTable: false,
+        docPinSelect: false,
+        markTable: false,
+        markSelect: false
       };
 
       $scope.headers = [
@@ -19,8 +25,13 @@ angular.module('admissionSystemApp')
         {name: 'docNum', display: 'номер документу'},
         {name: 'docDate', display: 'дата видачі'},
         {name: 'docIssued', display: 'ким видано'},
+        {name: 'isChecked', display: 'звірен з оригіналом'},
+        {name: 'isForeign', display: 'іноземного зразка'},
         {name: 'publicActivityTypeId', display: 'категорія нагорд'},
-        {name: 'awardName', display: 'нагорода'}
+        {name: 'publicActivityAwardId', display: 'нагорода'},
+        {name: 'honorsTypeId', display: 'тип відзнаки'},
+        {name: 'mark', display: 'середній бал'},
+        {name: 'docPin', display: 'пін-код'}
       ];
 
       /* function that's making decoding in data */
@@ -39,7 +50,8 @@ angular.module('admissionSystemApp')
       var paperTypeNames = [],
         paperUsageNames = [],
         publicAwards = [],
-        publicActivities = [];
+        publicActivities = [],
+        decodeHonorTypes = [];
 
       /* getting from service the categories of papers */
       DictionariesSvc.getPaperUsages().then(function (paperUsage) {
@@ -48,27 +60,24 @@ angular.module('admissionSystemApp')
       });
 
       /* function that's setting the children in our category of papers(if category have chosen, return all children from this category) */
-      $scope.setNewData = function (id) {
-        DictionariesSvc.getPaperTypes().then(function (paperType) {
-          pushData(paperType, paperTypeNames);
-          $scope.newData = [];
-          for (var i = 0; i < paperType.length; i++) {
-            if (paperType[i].paperUsageId === id) {
-              $scope.newData.push(paperType[i]);
-            }
-          }
-        });
-      };
+      DictionariesSvc.getPaperTypes().then(function (paperType) {
+        pushData(paperType, paperTypeNames);
+        $scope.newData = paperType;
+      });
 
-      //DictionariesSvc.getPaperTypes().then(function (paperType) {
-      //  pushData(paperType, paperTypeNames);
-      //  $scope.newData = paperType;
-      //});
+      $scope.filterPaperTypes = function (paper) {
+        return paper.paperUsageId === $scope.currentObj.abbrName;
+      };
 
       /* getting from service the categories of awards */
       DictionariesSvc.getPublicActivities().then(function (publicActiv) {
         $scope.publicActiv = publicActiv;
         pushData(publicActiv, publicActivities);
+      });
+
+      DictionariesSvc.getHonorsTypes().then(function (honorTypes) {
+        $scope.honorTypes = honorTypes;
+        pushData(honorTypes, decodeHonorTypes);
       });
 
       /* function that's setting the children in our category of awards(if category have chosen, return all children from this category) */
@@ -79,49 +88,60 @@ angular.module('admissionSystemApp')
         });
       };
 
-      $scope.currentObj = {};
       $scope.entirePerson.papers = [];
       $scope.inputData = [];
+      $scope.currentObj = {};
+      $scope.currentObj.pickAward = {};
+      $scope.currentObj.isChecked = 0;
+      $scope.currentObj.isForeign = 0;
 
       var cloneView,
         cloneViewDecode,
-        award = {},
         cloneMainNotDecode;
 
       /* the function that's adding the object to the row of table  */
       $scope.addToTable = function () {
         $scope.currentObj.docDate = $filter('date')($scope.currentObj.docDate, 'yyyy-MM-dd');
         cloneMainNotDecode = _.clone($scope.currentObj);
-        cloneMainNotDecode.award = award;
-        award.awardName = cloneMainNotDecode.awardName;
-        award.publicActivityTypeId = cloneMainNotDecode.publicActivityTypeId;
-        delete cloneMainNotDecode.awardName;
+        cloneMainNotDecode.award = {};
+        //cloneMainNotDecode.award.publicActivityAwardId = cloneMainNotDecode.publicActivityAwardId;
+        cloneMainNotDecode.award.publicActivityAwardId = cloneMainNotDecode.pickAward.id;
+        cloneMainNotDecode.award.bonus = cloneMainNotDecode.pickAward.bonus;
+        delete cloneMainNotDecode.pickAward;
         delete cloneMainNotDecode.publicActivityTypeId;
-        //delete cloneMainNotDecode.abbrName;
+        delete cloneMainNotDecode.abbrName;
         $scope.entirePerson.papers.push(cloneMainNotDecode);
 
         cloneView = _.clone($scope.currentObj);
         cloneViewDecode = decodeData(cloneView);
-        cloneView.award = award;
-        award.awardName = cloneViewDecode.awardName;
-        award.publicActivityTypeId = cloneViewDecode.publicActivityTypeId;
-        delete cloneViewDecode.awardName;
+        cloneView.award = {};
+        cloneView.award.publicActivityTypeId = cloneViewDecode.publicActivityTypeId;
+        cloneView.award.publicActivityAwardId = cloneViewDecode.pickAward.awardName;
+        cloneView.award.bonus = cloneViewDecode.pickAward.bonus;
+        delete cloneViewDecode.pickAward;
         delete cloneViewDecode.publicActivityTypeId;
-        //delete cloneViewDecode.abbrName;
+        delete cloneViewDecode.abbrName;
         $scope.inputData.push(cloneViewDecode);
 
         $scope.isVisible.isAddToTable = true;
         $scope.isVisible.isSaveToTable = false;
         $scope.currentObj = {};
-        //_.merge(objToEditDecoded, decodeData($scope.currentObj));
+        $scope.currentObj.pickAward = {};
+        $scope.currentObj.isChecked = 0;
+        $scope.currentObj.isForeign = 0;
 
+        $scope.isVisible.publicActiveSelect = false;
+        $scope.isVisible.docPinSelect = false;
+        $scope.isVisible.studySelect = false;
+        $scope.isVisible.markSelect = false;
       };
 
       function decodeData(obj) {
         obj.abbrName = paperUsageNames[obj.abbrName];
         obj.paperTypeId = paperTypeNames[obj.paperTypeId];
         obj.publicActivityTypeId = publicActivities[obj.publicActivityTypeId];
-        obj.awardName = publicAwards[obj.awardName];
+        obj.publicActivityAwardId = publicAwards[obj.publicActivityAwardId];
+        obj.honorsTypeId = decodeHonorTypes[obj.honorsTypeId];
         return obj;
       }
 
@@ -137,13 +157,32 @@ angular.module('admissionSystemApp')
 
       /* the function that's editing the object, and let's to change current data in this object  */
       $scope.editData = function (item, idx) {
+        $scope.isVisible.publicActiveSelect = false;
+        $scope.isVisible.docPinSelect = false;
+        $scope.isVisible.studySelect = false;
+        $scope.isVisible.markSelect = false;
 
         objToEdit = $scope.entirePerson.papers[idx];
         _.merge($scope.currentObj, objToEdit);
         objToEditDecoded = $scope.inputData[idx];
-        $scope.addSelect(objToEdit.paperTypeId);
+        $scope.addSelect(objToEdit);
         $scope.isVisible.isAddToTable = false;
         $scope.isVisible.isSaveToTable = true;
+
+        angular.forEach($scope.newData, function (item) {
+          if (item.id === objToEdit.paperTypeId) {
+            $scope.currentObj.abbrName = item.paperUsageId;
+            return false;
+          }
+        });
+
+        if (objToEdit.award.publicActivityAwardId < 64) {
+          $scope.currentObj.publicActivityTypeId = 1;
+          $scope.setAdditionalData($scope.currentObj.publicActivityTypeId);
+        } else if (objToEdit.award.publicActivityAwardId >= 64) {
+          $scope.currentObj.publicActivityTypeId = 2;
+          $scope.setAdditionalData($scope.currentObj.publicActivityTypeId);
+        }
       };
 
       /* the function that's saving the object, and let's to update the changes  */
@@ -152,19 +191,48 @@ angular.module('admissionSystemApp')
         _.merge(objToEdit, $scope.currentObj);
         _.merge(objToEditDecoded, decodeData($scope.currentObj));
         $scope.currentObj = {};
-        delete cloneViewDecode.awardName;
-        delete cloneViewDecode.publicActivityTypeId;
+        $scope.currentObj.pickAward = {};
+        $scope.currentObj.isChecked = 0;
+        $scope.currentObj.isForeign = 0;
+
+        objToEdit.award.publicActivityAwardId = objToEdit.pickAward.id;
+        objToEdit.award.bonus = objToEdit.pickAward.bonus;
+        delete objToEdit.abbrName;
+        delete objToEdit.pickAward;
+        delete objToEdit.publicActivityTypeId;
+
+        objToEditDecoded.award.publicActivityTypeId = objToEditDecoded.publicActivityTypeId;
+        objToEditDecoded.award.publicActivityAwardId = objToEditDecoded.pickAward.awardName;
+        objToEditDecoded.award.bonus = objToEditDecoded.pickAward.bonus;
+        delete objToEditDecoded.abbrName;
+        delete objToEditDecoded.pickAward;
+        delete objToEditDecoded.publicActivityTypeId;
+
         $scope.isVisible.isAddToTable = true;
         $scope.isVisible.isSaveToTable = false;
+
+        $scope.isVisible.publicActiveSelect = false;
+        $scope.isVisible.docPinSelect = false;
+        $scope.isVisible.studySelect = false;
+        $scope.isVisible.markSelect = false;
       };
 
       /* the function that's adding the select with children of chosen category  */
-      $scope.addSelect = function (paperTypeId) {
-        if (paperTypeId === 29) {
-          $scope.isVisible.publicActivSelect = true;
+      $scope.addSelect = function (paper) {
+        if (paper.paperTypeId === 29) {
+          $scope.isVisible.publicActiveSelect = true;
           $scope.isVisible.publicActiveTable = true;
-        } else {
-          $scope.isVisible.publicActivSelect = false;
+        } else if (paper.paperTypeId === 4) {
+          $scope.isVisible.docPinTable = true;
+          $scope.isVisible.docPinSelect = true;
+          $scope.isVisible.markTable = true;
+          $scope.isVisible.markSelect = true;
+        } else if (_.contains([2, 7, 9, 10, 11, 12, 13, 22, 23], paper.paperTypeId)) {
+          $scope.isVisible.studyTable = true;
+          $scope.isVisible.studySelect = true;
+        } else if (_.contains([30, 54], paper.paperTypeId)) {
+          $scope.isVisible.markTable = true;
+          $scope.isVisible.markSelect = true;
         }
       };
     }]);
